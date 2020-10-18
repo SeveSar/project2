c9e66856fdf28e0336ff8cef8a68d064dbf684f5
caesar <caesar@mail.ru>
2020-10-18 18:41:16 +0500

    commit_18_10_20_1

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
 
     $('ul.btn-tabs').on('click','li:not(.btn-tabs__item_active)', function(){
-        console.log('click');
+    
         $(this)
         .addClass('btn-tabs__item_active').siblings().removeClass('btn-tabs__item_active')
         .closest("article.ideas__item").find("div.ideas__item-descr").hide(700)
@@ -11,7 +11,7 @@ $(document).ready(function(){
     });
 
     $('ul.btn-tabs').on('click','li:not(.btn-tabs__item_active)', function(){
-        console.log('click');
+       
         $(this)
         .addClass('btn-tabs__item_active').siblings().removeClass('btn-tabs__item_active')
         .closest("div.container").find("div.work__catalog").hide(700)
@@ -20,8 +20,27 @@ $(document).ready(function(){
         
     });
 
+    let promoH = $('.promo').innerHeight();
+    let scrollPos = $(window).scrollTop();
+    checkScrollPos (promoH, scrollPos);
+
+    $(window).on('scroll resize', function () {
+        promoH = $('.promo').innerHeight();
+        scrollPos = $(this).scrollTop();
+        checkScrollPos (promoH, scrollPos);
+        console.log(scrollPos);
+        
+    });
+
 
     
+    function checkScrollPos (promoH, scrollPos) {
 
+        if (scrollPos >= promoH){
+            $(".header").addClass('fixed');
+        } else {
+            $(".header").removeClass('fixed');
+        }
+    }
     
 });
