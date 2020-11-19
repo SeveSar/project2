de968e5e0a494cc13d76c60f6de9e4b2d6ce0a63
caesar <caesar@mail.ru>
2020-11-19 15:21:54 +0500

    19_11_20_1

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+// document.documentElement.style.setProperty('--animate-duration', '10s');
+
 $(document).ready(function(){
 
     $('ul.btn-tabs').on('click','li:not(.btn-tabs__item_active)', function(){
@@ -20,27 +22,65 @@ $(document).ready(function(){
         
     });
 
-    let promoH = $('.promo').innerHeight();
-    let scrollPos = $(window).scrollTop();
-    checkScrollPos (promoH, scrollPos);
+    // fixed header with JQUERY
+    // let promoH = $('.promo').innerHeight();
+    // let scrollPos = $(window).scrollTop();
+    // checkScrollPos (promoH, scrollPos);
 
-    $(window).on('scroll resize', function () {
-        promoH = $('.promo').innerHeight();
-        scrollPos = $(this).scrollTop();
-        checkScrollPos (promoH, scrollPos);
-        console.log(scrollPos);
+    // $(window).on('scroll resize load', function () {
+    //     promoH = $('.promo').innerHeight();
+    //     scrollPos = $(this).scrollTop();
+        
+    //     checkScrollPos (promoH, scrollPos);
+
+        
+    // });
+
+
+    
+    // function checkScrollPos (promoH, scrollPos) {
+
+    //     if (scrollPos > promoH){
+    //         $(".header").addClass('fixed');
+    //     } else {
+    //         $(".header").removeClass('fixed');
+    //     }
+    // }
+
+    $('a[data-scroll]').on('click', function() {
+        let scrollId =  $(this).attr('data-scroll');
+        let offsetTopItem = $(scrollId).offset().top;
+        $('html, body').animate({scrollTop: offsetTopItem - 50}, 700);
         
     });
 
-
-    
-    function checkScrollPos (promoH, scrollPos) {
-
-        if (scrollPos >= promoH){
-            $(".header").addClass('fixed');
-        } else {
-            $(".header").removeClass('fixed');
-        }
-    }
+    $('button[type = "submit"]').on('click', function(e) {
+        e.preventDefault();
+        
+    });
     
 });
+
+let promoH = document.querySelector('.promo').offsetHeight;
+let scrollPosition = window.scrollY;
+let header = document.querySelector('.header');
+checkScroll(scrollPosition, promoH)
+
+window.addEventListener('scroll', function() {
+    promoH = document.querySelector('.promo').offsetHeight;
+    scrollPosition = window.scrollY;
+    checkScroll(scrollPosition, promoH);
+});
+window.addEventListener('resize', function() {
+    promoH = document.querySelector('.promo').offsetHeight;
+    scrollPosition = window.scrollY;
+    checkScroll(scrollPosition, promoH);
+});
+
+function checkScroll(scrollPosition, promoH){
+    if (scrollPosition > promoH){
+        header.classList.add('fixed');
+    } else {
+        header.classList.remove('fixed');
+    }
+};
